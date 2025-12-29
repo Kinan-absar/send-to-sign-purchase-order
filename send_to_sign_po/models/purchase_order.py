6df46b84674ac346ef065e4268f6628a82cb3a1a
Kinan-absar <kinan@absar-alomran.com>
2025-12-29 14:59:09 +0300

    Update purchase_order.py

--- send_to_sign_po/models/purchase_order.py
+++ send_to_sign_po/models/purchase_order.py
@@ -16,7 +16,11 @@ class PurchaseOrder(models.Model):
     ], default="draft", tracking=True)
 
     revision = fields.Integer(default=0, tracking=True)
-
+    
+    project_id = fields.Many2one(
+        'project.project',
+        string='Project',
+        tracking=True,
     # ---------------------------------------------------------------------
     # WRITE OVERRIDE – Reset signature workflow when PO is modified
     # ---------------------------------------------------------------------
